2c298a28e7605e9f4df2bf206f92dc9e9c6ba788
hugohtz03 <hugoheintz03@gmail.com>
2024-02-15 12:15:00 +0100

    Echauffement

---
+++ feu00.js
@@ -0,0 +1,44 @@
+// Fonction utilisée
+function square(largeur, hauteur) {
+    
+    if (largeur === 1) {
+        console.log('o');
+        
+        for (let i = 1; i < hauteur - 1; i++) {
+            console.log('|');
+        }
+
+        if (hauteur > 1) {
+            console.log('o');
+        }
+    } else {
+        const ligneLargeur = 'o' + '-'.repeat(largeur - 2) + 'o';
+        console.log(ligneLargeur);
+
+        for (let i = 0; i < hauteur - 2; i++) {
+            const ligneHauteur = '|' + ' '.repeat(largeur - 2) + '|';
+            console.log(ligneHauteur);
+        }
+
+        if (hauteur > 1) {
+            console.log(ligneLargeur);
+        }
+    }
+}
+
+// Parsing
+function getArguments() {
+    let firstArgument = process.argv[2];
+    let secondArgument = process.argv[3];
+
+    firstArgument = parseInt(firstArgument);
+    secondArgument = parseInt(secondArgument);
+
+    return [firstArgument, secondArgument];
+}
+
+// Résolution
+let arguments = getArguments();
+
+// Affichage
+square(arguments[0], arguments[1]);
