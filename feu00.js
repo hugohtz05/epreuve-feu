60579cb9746fa2e07acdc25bf6e5a91f77bf5a87
hugohtz03 <hugoheintz03@gmail.com>
2024-05-30 21:03:02 +0200

    echaufement

--- feu00.js
+++ feu00.js
@@ -1,50 +1,47 @@
 // Fonction utilisée
 function square(largeur, hauteur) {
-    
-    if (largeur === 1) {
-        console.log('o');
-        
-        for (let i = 1; i < hauteur - 1; i++) {
-            console.log('|');
-        }
+  if (largeur === 1) {
+    console.log("o");
 
-        if (hauteur > 1) {
-            console.log('o');
-        }
-    } else {
-        const ligneLargeur = 'o' + '-'.repeat(largeur - 2) + 'o';
-        console.log(ligneLargeur);
-
-        for (let i = 0; i < hauteur - 2; i++) {
-            const ligneHauteur = '|' + ' '.repeat(largeur - 2) + '|';
-            console.log(ligneHauteur);
-        }
-
-        if (hauteur > 1) {
-            console.log(ligneLargeur);
-        }
+    for (let i = 1; i < hauteur - 1; i++) {
+      console.log("|");
     }
+
+    if (hauteur > 1) {
+      console.log("o");
+    }
+  } else {
+    const ligneLargeur = "o" + "-".repeat(largeur - 2) + "o";
+    console.log(ligneLargeur);
+
+    for (let i = 0; i < hauteur - 2; i++) {
+      const ligneHauteur = "|" + " ".repeat(largeur - 2) + "|";
+      console.log(ligneHauteur);
+    }
+
+    if (hauteur > 1) {
+      console.log(ligneLargeur);
+    }
+  }
 }
 
 // Parsing
 function getArguments() {
-    let firstArgument = process.argv[2];
-    let secondArgument = process.argv[3];
+  let firstArgument = process.argv[2];
+  let secondArgument = process.argv[3];
 
-    firstArgument = parseInt(firstArgument);
-    secondArgument = parseInt(secondArgument);
+  firstArgument = parseInt(firstArgument);
+  secondArgument = parseInt(secondArgument);
 
-    return [firstArgument, secondArgument];
+  return [firstArgument, secondArgument];
 }
 
-// Résolution
-function resolution(){
-    let arguments = getArguments();
-    square(arguments[0], arguments[1]);
-    return arguments;
+// Resolution
+function resolution() {
+  let arguments = getArguments();
+  square(arguments[0], arguments[1]);
+  return arguments;
 }
 
-
-// Affichage
+// Affiche
 resolution();
-
